14523cfafc6d85fcc671ebe00ac3927491481bf4
Admin <Admin@ADMIN-PC>
2019-10-13 20:02:16 +0200

    full commit

---
+++ FilterAttribute/DynamicObjHelper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace FilterAttribute
+{
+
+    class DynamicObjHelper
+    {
+        public static dynamic BuildDynamicObj(Dictionary<string, object> properties)
+        {
+            return new DynamicObj(properties);
+        }
+
+        public sealed class DynamicObj : DynamicObject
+        {
+            private readonly Dictionary<string, object> _properties;
+
+            public DynamicObj(Dictionary<string, object> properties)
+            {
+                _properties = properties;
+            }
+
+            public override IEnumerable<string> GetDynamicMemberNames()
+            {
+                return _properties.Keys;
+            }
+
+            public override bool TryGetMember(GetMemberBinder binder, out object result)
+            {
+                if (_properties.ContainsKey(binder.Name))
+                {
+                    result = _properties[binder.Name];
+                    return true;
+                }
+                else
+                {
+                    result = null;
+                    return false;
+                }
+            }
+
+            //public override bool TrySetMember(SetMemberBinder binder, object value)
+            //{
+            //    if (_properties.ContainsKey(binder.Name))
+            //    {
+            //        _properties[binder.Name] = value;
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+        }
+    }
+}
---
+++ FilterAttribute/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Dynamic;
+using System.Linq;
+using System.Reflection;
+
+namespace FilterAttribute
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+            var test = new MainList() {
+                ID=22,
+                Name="ehsan",
+                LastName="Manouchehri"
+            };
+            
+            var dic = new Dictionary<string,object>();
+           foreach (var p in test.GetType().GetProperties())
+            {
+                if (p.GetCustomAttributes(typeof(DuplicateKeyAttribute), false).Count() > 0)
+                {
+
+                    //just get the value of the property & cast it.
+                    var propertyValue = p.GetValue(test, null);
+                    dic.Add(p.Name, p.GetValue(test, null));
+                    
+                }
+            }
+            var dyn =DynamicObjHelper.BuildDynamicObj(dic);
+            foreach (var d in dic)
+            {
+                Console.WriteLine(d.Key+d.Value);
+                Console.WriteLine(d);
+            }
+            Console.WriteLine(dyn.ID);
+            Console.WriteLine(test.ID);
+
+        }
+        public class MainList
+        {
+            [DuplicateKey]
+            public int ID { get; set; }
+            [DuplicateKey]
+            public string Name { get; set; }
+            
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public int BankAcc { get; set; }
+
+        }
+    }
+   
+    internal class DuplicateKeyAttribute : Attribute
+    {
+    }
+   
+}
